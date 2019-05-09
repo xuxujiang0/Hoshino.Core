254026111d281f3ca096d4d22ee8dffce02e5190
river.xu <1842125096@qq.com>
2019-05-09 11:07:09 +0800

    更新框架

---
+++ Hoshino.Core.Common/Hoshino.Core.Common/Layout/DBLogLayout.cs
@@ -0,0 +1,49 @@
+﻿using log4net.Core;
+using log4net.Layout;
+using log4net.Layout.Pattern;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Hoshino.Core.Common.Layout
+{
+    public class DBLogLayout : PatternLayout
+    {
+        public DBLogLayout()
+        {
+            this.AddConverter("property", typeof(DBLogLayoutConverter));
+        }
+    }
+
+    public class DBLogLayoutConverter : PatternLayoutConverter
+    {
+        protected override void Convert(TextWriter writer, LoggingEvent loggingEvent)
+        {
+            if (Option != null)
+            {
+                // Write the value for the specified key
+                WriteObject(writer, loggingEvent.Repository, LookupProperty(Option, loggingEvent));
+            }
+            else
+            {
+                // Write all the key value pairs
+                WriteDictionary(writer, loggingEvent.Repository, loggingEvent.GetProperties());
+            }
+        }
+
+        private object LookupProperty(string property, log4net.Core.LoggingEvent loggingEvent)
+        {
+            object propertyValue = string.Empty;
+
+            PropertyInfo propertyInfo = loggingEvent.MessageObject.GetType().GetProperty(property);
+            if (propertyInfo != null)
+            {
+                propertyValue = propertyInfo.GetValue(loggingEvent.MessageObject, null);
+            }
+
+            return propertyValue;
+        }
+    }
+}
